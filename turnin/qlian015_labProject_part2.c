85ec77f620efa10692df7116dd295503124f02c8
2000rick <qipeng.liang.edu@gmail.com>
2021-11-28 15:59:48 -0800

    part2 demo link added

--- turnin/qlian015_labProject_part2.c
+++ turnin/qlian015_labProject_part2.c
@@ -8,7 +8,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo Link: 
+ *	Demo Link: https://youtu.be/xe0uIFK9Fqw 
  */
 
 #include <avr/io.h>
